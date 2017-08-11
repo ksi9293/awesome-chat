c7bfbab37974c14ac266f0edc7ad2f744d1646fe
KSI <ksi9293@naver.com>
2017-08-11 17:21:31 +0900

    잡문자 삭제

--- server.js
+++ server.js
@@ -1,12 +1,8 @@
 const http = require('http');
 
-<<<<<<< HEAD
 const hostname = '127.0.0.1';
 const port = 1337;
-=======
-const hostname = 'http://localhost';
-const port = 80;
->>>>>>> 8c64d36c7b85dba2f59cb431d1250d4248aa7c50
+
 
 http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain'});
